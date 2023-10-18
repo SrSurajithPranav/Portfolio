669843e0f2b28789a5214879a3fd185032b807bd
SrSurajithPranav <91361858+SrSurajithPranav@users.noreply.github.com>
2023-10-19 00:36:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const navItems = document.querySelectorAll('.nav-item');
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+});
 
-navItems.forEach((navItem) => {
-  navItem.addEventListener('click', () => {
-    const content = navItem.nextElementSibling;
-    content.style.display = 'block';
-  });
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function(){
+  mobileNav.classList.add('open');  
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');  
+});
+
 });
